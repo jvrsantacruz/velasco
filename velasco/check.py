01c67e6d8424fa017397e88ed46bcb0ea729369c
Javier Santacruz <javier.santacruz@avature.net>
2017-04-03 16:47:21 +0200

    Updates check script to validate conditions on metadata also

--- velasco/check.py
+++ velasco/check.py
@@ -1,17 +1,51 @@
 # -*- coding: utf-8 -*-
 import argparse
+from collections import defaultdict
 
-from parsing import Book, parse_mentions, write
+from parsing import Book, parse_mentions, parse_metadata, write
 
 
-HEADER = ('Inventario', 'Posición', 'Problema', 'Título')
+HEADER = ('id', 'inventario', 'posicion', 'problema', 'titulo')
 
 
 def problem(m, msg):
-    return dict(zip(HEADER, (m['list_id'] + 1, m['pos'] + 1, msg, m['title'])))
+    if 'book_id' in m:
+        return dict(zip(
+            HEADER, (m['book_id'], m['list_id'] + 1, m['pos'] + 1, msg, m['title'])
+        ))
+    else:
+        return dict(zip(
+            HEADER, (m['id'], 'meta', '', msg, m['title'])
+        ))
 
 
-def find_problems(mentions):
+def repeated(sequence, field):
+    byfield = defaultdict(list)
+    [byfield[e[field]].append(e) for e in sequence]
+    for name, entries in byfield.items():
+        if name and len(entries) > 1:
+            yield name, entries
+
+
+def find_repes(sequence, field):
+    for ref, entries in repeated(sequence, field):
+        repes = ', '.join('{id}: ({title})'.format(**e) for e in entries)
+        yield problem(
+            entries[0],
+            'El campo {} {!r} está repetido en varios libros: {}'
+            .format(field, ref, repes)
+        )
+
+
+def find_meta_problems(meta):
+    meta = list(meta)
+    yield from find_repes(meta, 'ref')
+    yield from find_repes(meta, 'ref_old')
+    yield from find_repes(meta, 'id')
+    yield from find_repes(meta, 'idx')
+
+
+def find_mention_problems(mentions):
     books = {}
     for m in mentions:
         bid = m['book_id']
@@ -21,25 +55,33 @@ def find_problems(mentions):
             yield problem(m, 'No tiene ID')
 
         elif m['list_id'] == other.get('list_id'):
-            yield problem(m, 'Repetido en el mismo inventario {!r}'.format(other))
-
+            yield problem(
+                m, 'Repetido en el mismo inventario {!r}'.format(other)
+            )
         else:
             books[bid] = m
 
-        # chars = set(m['title']).intersection('().,!?¿¡')
-        # if chars:
-        #     problem(m, 'Signos de puntuación en el título: {!r}'
-        #             .format(' '.join(chars)))
+        chars = set(m['title']).intersection('().,!?¿¡')
+        if chars:
+            problem(m, 'Signos de puntuación en el título: {!r}'
+                    .format(' '.join(chars)))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('inventarios')
+    parser.add_argument('metadata')
     parser.add_argument('-o', '--output')
     parser.add_argument('-f', '--format')
     args = parser.parse_args()
 
-    problems = find_problems(parse_mentions(Book(args.inventarios)))
+    problems = []
+    problems.extend(
+        find_mention_problems(parse_mentions(Book(args.inventarios)))
+    )
+    problems.extend(
+        find_meta_problems(parse_metadata(args.metadata))
+    )
     write(problems, args.output, header=HEADER, format=args.format)
 
 
