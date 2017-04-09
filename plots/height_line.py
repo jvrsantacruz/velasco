752211fbbd26d1167f4b56f485ade8c7aba70c44
Javier Santacruz <javier.santacruz@avature.net>
2017-04-10 01:37:40 +0200

    Fixes labels and ticks for height line plot

--- plots/height_line.py
+++ plots/height_line.py
@@ -1,6 +1,8 @@
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from matplotlib.colors import ListedColormap
 
 from common import get_args, plotting, legend, set_axis, read_table
@@ -9,14 +11,25 @@ from common import get_args, plotting, legend, set_axis, read_table
 def main():
     args = get_args(__file__)
 
-    f, ax = plt.subplots()
     # TODO fix axis
-    pd.DataFrame(read_table(args.table))\
+    df = pd.DataFrame(read_table(args.table))\
         .pivot(index='pos', columns='lid')\
-        .fillna(float('NaN'))\
-        .height\
-        .plot\
-        .bar(subplots=True)
+        .fillna(float('NaN'))
+
+    title = 'Altura de libro por inventario y posición'
+
+    plots = df.height\
+        .plot(kind='bar', subplots=True, title=title, grid=True)
+
+    for plot in plots:
+        plot.set_title('')
+        plot.set_ylabel('')
+        plot.legend(loc='upper right', bbox_to_anchor=(1.1, 1))
+
+    visible = set(range(1, len(df), 5))
+    for n, label in enumerate(plots[-1].xaxis.get_ticklabels()):
+        label.set_visible(n + 1 in visible)
+
     plotting(plt, args)
 
 
