2b6096cea7b366a2f9e82248a12678558cbd76ca
Javier Santacruz <javier.santacruz@avature.net>
2017-04-10 22:45:39 +0200

    Paralellize plotall.sh script

--- plotall.sh
+++ plotall.sh
@@ -8,19 +8,69 @@ declare -r DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 declare -r DIR="$NAME-$DATE"
 declare -r TABLE="$DIR/table.csv"
 declare -r BNE_TABLE="$DIR/bne.csv"
+declare -r NTASKS=8
+
+
+function plot_simple {
+    local name=$1
+    python plots/${name}.py $TABLE --save --output $DIR --ext png --dpi 200 &
+}
+
+
+function plot_orden {
+    local first=$1
+    local second=$2
+
+    python plots/orden_orden.py $TABLE\
+        --ext png --dpi 200 --save --output $DIR\
+        --first $first --second $second\
+        --name "orden_orden_${first}_vs_${second}_simple"
+
+    python plots/orden_orden.py $TABLE\
+        --ext png --dpi 200 --save --output $DIR\
+        --first $first --second $second --annotated\
+        --name "orden_orden_${first}_vs_${second}_annotated"
+
+    for criteria in topic lang height area material author; do
+        python plots/orden_orden.py $TABLE\
+            --ext png --dpi 200 --save --output $DIR\
+            --first $first --second $second --color-by $criteria\
+            --name "orden_orden_${first}_vs_${second}_${criteria}"
+
+        python plots/orden_orden.py $TABLE\
+            --ext png --dpi 200 --save --output $DIR\
+            --first $first --second $second --color-by $criteria --annotated\
+            --name "orden_orden_${first}_vs_${second}_${criteria}_annotated"
+    done
+}
+
+
+function wait_some {
+    while test "$(jbos -p | wc -l)" -gt $NTASKS; do
+        wait "$(jobs -p | head -n 1)"  || true
+    done
+}
+
+function wait_all {
+    for pid in $(jobs -p); do wait $pid; done
+}
+
 
 mkdir -p $DIR
 python velasco/bne.py "$METADATA" "$BNE" --output $BNE_TABLE
 python velasco/table.py "$INVENTARIOS" "$METADATA" "$BNE_TABLE" --output $TABLE
 
 for name in todos tema idioma orden orden_uno fantasmas area height; do
-    python plots/${name}.py $TABLE --save --output $DIR --ext png --dpi 200
+    plot_simple "$name" &
 done
+wait_some
+
 for first in $(seq 1 5); do
     for second in $(seq 1 5); do
         if test $first -ne $second; then
-            python plots/orden_orden.py $TABLE --output $DIR --ext png --dpi 200 \
-                --first $first --second $second --save
+            plot_orden "$first" "$second" &
         fi
     done
+    wait_some
 done
+wait_all
